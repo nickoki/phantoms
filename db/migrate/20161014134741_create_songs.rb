fdb6a37eba57ced907adab5e000b2aa4200a9d7c
Nick Oki <nick.oki@live.com>
2016-10-14 09:58:23 -0400

    generate, migrate Songs

---
+++ db/migrate/20161014134741_create_songs.rb
@@ -0,0 +1,12 @@
+class CreateSongs < ActiveRecord::Migration[5.0]
+  def change
+    create_table :songs do |t|
+      t.string :title
+      t.string :artist
+      t.datetime :debut_date
+      t.boolean :is_active
+
+      t.timestamps
+    end
+  end
+end
